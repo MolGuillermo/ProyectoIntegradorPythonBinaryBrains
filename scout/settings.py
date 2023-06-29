3938134006b4b64b3ba8302c3240a538c17d26cb
Tamara Piccinni <103544686+TamaraPiccinni@users.noreply.github.com>
2023-06-29 02:00:53 -0300

    cambios footer
    Co-authored-by: Carolina Riddick <Carolina-Riddick@users.noreply.github.com>

--- scout/settings.py
+++ scout/settings.py
@@ -126,3 +126,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# #para agregar el favicon segun chatgpt, debo agregar este codigo para que pueda leerlo
+#  STATICFILES_DIRS = [
+#      os.path.join(BASE_DIR, 'static'),
+# ]#ver linea 9 y 10 de index.html ahi agregue codigo.
+
+
+
+
+
+
